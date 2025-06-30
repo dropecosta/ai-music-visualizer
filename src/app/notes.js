43b91577d728098244451b4e87276058d8ef4cb8
Pedro Reis <dropecosta@gmail.com>
2025-06-30 09:43:27 +0100

    Adding freqToMidi function

--- src/app/notes.js
+++ src/app/notes.js
@@ -15,3 +15,34 @@ export default function Notes() {
     </div>
   );
 }
+
+// ---------------------------- HELPER FUNCTIONS ----------------------------
+/*  
+ name: startAudioContext
+input: none
+ desc: provides user to allow for audio input on the web
+*/
+function startAudioContext() {
+  if (audioContext) {
+    // if the AudioContext is already created, resume it
+    audioContext.resume();
+  } else {
+    // create and start the AudioContext from browser
+    audioContext = new (window.AudioContext ||
+      window.webkitAudioContext ||
+      window.mozAudioContext ||
+      window.oAudioContext ||
+      window.msAudioContext)();
+  }
+}
+
+/*  
+ name: freqToMidi
+input: f (frequency)
+ desc: converts frequency input from audio to a MIDI number
+*/
+function freqToMidi(f) {
+  const mathlog2 = Math.log(f / 440) / Math.log(2);
+  const m = Math.round(12 * mathlog2) + 69;
+  return m;
+}

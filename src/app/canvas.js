d56389d9eb0f0eed1a13b568154ad441569c1f3a
Pedro Reis <dropecosta@gmail.com>
2025-06-30 09:42:25 +0100

    Adding utils, starting with audio context and handling audio frequences

--- src/app/canvas.js
+++ src/app/canvas.js
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState } from 'react';
-import { useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import p5 from 'p5';
 
 export default function Canvas(note) {
     let canvasRef = useRef(null);
@@ -9,6 +9,50 @@ export default function Canvas(note) {
     const [color, setColor] = useState([0, 255, 0]);
     const canvasWidth = 800;
     const canvasHeight = 400;
+
+    useEffect(() => {
+        const sketch = (p) => {
+            p.setup = () => {
+                p.createCanvas(canvasWidth, canvasHeight);
+                p.clear();
+            };
+
+            p.draw = () => {
+                p.clear();
+                //if (note) {
+                    let xPos = mapNote(note.note);
+                //}
+                p.fill(color);
+                p.ellipse(xPos + canvasHeight / 2, 100, 100);
+            };
+
+            return () => {
+                canvas.remove();
+            };
+        };
+
+        const mapNote = (note) => {
+            const spacing = canvasWidth / 13;
+            const notePositions = {
+                'C': 0 * spacing,
+                'C#': 1 * spacing,
+                'D': 2 * spacing,
+                'D#': 3 * spacing,
+                'E': 4 * spacing,
+                'F': 5 * spacing,
+                'F#': 6 * spacing,
+                'G': 7 * spacing,
+                'G#': 8 * spacing,
+                'A': 9 * spacing,
+                'A#': 10 * spacing,
+                'B': 11 * spacing
+            };
+
+            return notePositions[note] || 0;
+        }
+
+        const canvas = new p5(sketch, canvasRef.current);
+    }, [note, color]);
 
     return (
         <div ref={canvasRef}></div>
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Notes from './notes';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <h1>Music Visualizer</h1>
       <Notes />
     </main>
